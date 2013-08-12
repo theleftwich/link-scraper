4a13983ab4b94c4c3c4a56ab596627a9616a8a37
theleftwich <leftwich@gmail.com>
2013-08-12 13:56:13 -0400

    first commit

---
+++ linkScraper03.py
@@ -0,0 +1,164 @@
+
+# Current state of this script
+# This script is able to log in to the CMS and will get all links matching
+# certain criteria from div#content and div#rightPortlets and write them 
+# to a CSV
+
+# don't forget to specify email addres for logging into Google Docs, paths and filenames to store your CSV!
+
+###############################################################################
+
+#get urls to scrape
+import re, urllib, urllib2
+
+# Download Google spreadsheet
+class Spreadsheet(object):
+    def __init__(self, key):
+        super(Spreadsheet, self).__init__()
+        self.key = key
+
+class Client(object):
+    def __init__(self, email, password):
+        super(Client, self).__init__()
+        self.email = email
+        self.password = password
+
+    def _get_auth_token(self, email, password, source, service):
+        url = "https://www.google.com/accounts/ClientLogin"
+        params = {
+            "Email": email, "Passwd": password,
+            "service": service,
+            "accountType": "HOSTED_OR_GOOGLE",
+            "source": source
+        }
+        req = urllib2.Request(url, urllib.urlencode(params))
+        return re.findall(r"Auth=(.*)", urllib2.urlopen(req).read())[0]
+
+    def get_auth_token(self):
+        source = type(self).__name__
+        return self._get_auth_token(self.email, self.password, source, service="wise")
+
+    def download(self, spreadsheet, gid=0, format="csv"):
+        url_format = "https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key=%s&exportFormat=%s&gid=%i"
+        headers = {
+            "Authorization": "GoogleLogin auth=" + self.get_auth_token(),
+            "GData-Version": "3.0"
+        }
+        req = urllib2.Request(url_format % (spreadsheet.key, format, gid), headers=headers)
+        return urllib2.urlopen(req)
+
+if __name__ == "__main__":
+    import getpass
+    import csv
+
+    email = "leftwich@umich.edu" 
+    password = getpass.getpass()
+	
+    spreadsheet_id = "0Ame82JMHWKQgdHdDQm02RW1kNVlKV2lLVlpDbG5aV3c" # (spreadsheet id here)
+
+    # Create client and spreadsheet objects
+    gs = Client(email, password)
+    ss = Spreadsheet(spreadsheet_id)
+
+    # Request a file-like object containing the spreadsheet's contents
+    csv_file = gs.download(ss)
+
+
+
+# login to CMS
+import mechanize
+import cookielib
+from bs4 import BeautifulSoup
+
+
+# Browser
+br = mechanize.Browser()
+
+# Cookie Jar
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
+
+# Browser options
+br.set_handle_equiv(True)
+br.set_handle_gzip(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+
+# Follows refresh 0 but not hangs on refresh > 0
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+# User-Agent (this is cheating, ok?)
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+# The site we will navigate into, handling it's session
+br.open('https://weblogin.umich.edu/')
+
+# Select the first (index zero) form
+br.select_form(nr=0)
+
+# User credentials
+br.form['login'] = 'leftwich'
+br.form['password'] = getpass.getpass()
+
+
+# Submit login
+br.submit()
+
+
+# Output
+# specify path to file and file variables
+from os.path import join as pjoin
+path_to_folder = pjoin('C:\\', 'Users', 'leftwich', 'Documents', 'scraper')
+filename = 'aeroLinkScraper_080713_2.csv'
+
+# open a file to write results in
+outputFile = open(path_to_folder + '\\' + filename, 'a')
+
+
+# now scrape for links
+# imports		
+import csv 
+import urllib2
+import re
+
+# Get urls from csv object we just downloaded and start loop
+urls = csv.reader(csv_file)
+
+
+# Open each page from Google Docs csv, read contents
+for url in urls:
+    response = br.open(url[0])
+    fullHTML = response.read()
+
+
+    
+	# put the HTML into BeautifulSoup
+    soup = BeautifulSoup(fullHTML)
+
+	
+	# zoom in to div#content, get content and store
+    content_div = soup.find('div',attrs={'id':'contentHolder'})
+	
+	
+	# zoom in to div#rightPortlets, get content and store
+    portlet_div = soup.find('div',attrs={'id':'rightPortlets'})
+	
+
+	# get all the links from div#content, write each to a line of a .csv file
+    if content_div is not None:
+        for link in content_div.findAll('a'):
+            h = link.get('href')
+            if h is not None and h.startswith('#')==False and 'mailto' not in h:
+                outputFile.write ('"' + (url[0]) + '","' + link.get('href') + '"' + '\n')
+	
+	
+	# get all the links from div#rightPortlets, write each to a line of a .csv file
+    if portlet_div is not None:
+        for link in content_div.findAll('a'):
+            h = link.get('href')
+            if h is not None and h.startswith('#')==False and 'mailto' not in h:
+                outputFile.write ('"' + (url[0]) + '","' + link.get('href') + '"' + '\n')
+
+outputFile.close()    
+
